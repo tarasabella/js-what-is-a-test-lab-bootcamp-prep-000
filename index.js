c54154cffc65d30e26e490ef89f219acc6fde7c4
Tara E. Sabella <tarasabella@gmail.com>
2018-10-02 02:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches tall`
+var message = ` ${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
